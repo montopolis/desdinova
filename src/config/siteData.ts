03e26d9b677ee415b276233f697e8bd1c6f62513
Babul A. Mukherjee <babul@montopolis.com>
2024-08-28 10:03:19 -0500

    cleanup of siteData

--- src/config/siteData.ts
+++ src/config/siteData.ts
@@ -1,13 +1,10 @@
 export interface SiteDataProps {
     url: string
     name: string
+    description: string
 
     company: string
     companyUrl: string
-
-    seo: string
-    title: string
-    description: string
 
     author: {
         name: string
@@ -24,29 +21,39 @@ export interface SiteDataProps {
         mainBackground: string
         activeMenuClass: string
         inactiveMenuClass: string
+        activeMenuIconClass: string
+        inactiveMenuIconClass: string
     }
 }
 // Update this file with your site specific information
 const siteData: SiteDataProps = {
-    url: "https://astro.build",
-    name: "Desdinova",
+    ////////////////////////////////////////
+    // UPDATE BELOW FIRST
+    ////////////////////////////////////////
 
+    // url should be your fully qualified public URL
+    url: "https://astro.build",
+    // your website name
+    name: "Desdinova",
+    // your website description, which is also default SEO text
+    description: "Built using Desdinova in Astro",
+
+    // company/organization & url for responsible party (see Footer & Privacy)
     company: "A Company",
     companyUrl: "https://a-company.com",
 
-    // Your website's title and description (meta fields)
-    seo: "seo text",
-    title: "Desdinova",
-    description: "Built using Desdinova in Astro",
+    ////////////////////////////////////////
+    // LEAVE EVERYTHING BELOW ALONE FOR NOW
+    ////////////////////////////////////////
 
-    // author not used
+    // NOT USED - author
     author: {
         name: "First Name",
         email: "email@organization.com",
         twitter: "@organization",
     },
 
-    // default image for meta tags if the page does not have an image already
+    // NOT USED - default image for meta tags if the page does not have an image already
     defaultImage: {
         src: "favicon.svg",
         alt: "Desdinova Logo",
@@ -55,10 +62,17 @@ const siteData: SiteDataProps = {
     colors: {
         // mainBackground is color of canvas behind everything else
         mainBackground: "bg-white dark:bg-gray-800",
+        // tailwind classes for active menu item
         activeMenuClass:
             "flex items-center justify-center py-2 pr-4 pl-3 text-black dark:text-white lg:text-primary-700",
+        // tailwind classes for inactive menu items
         inactiveMenuClass:
             "flex items-center justify-center py-2 pr-4 pl-3 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent text-gray-400 dark:text-gray-400",
+        // tailwind classes for active menu item icon
+        activeMenuIconClass: "mr-1 h-4 w-4 inline text-black dark:text-white",
+        // tailwind classes for inactive menu item icons
+        inactiveMenuIconClass:
+            "mr-1 h-4 w-4 inline text-gray-400 dark:text-gray-400",
     },
 }
 
