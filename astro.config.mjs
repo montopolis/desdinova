559852368a807de91d74abf7c9857f42080c726c
Babul A. Mukherjee <babul@montopolis.com>
2024-08-27 20:12:11 -0500

    moved site configuration variables from astro.config.mjs to site.config.mjs to improve dev performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,13 @@ import tailwind from "@astrojs/tailwind"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import icon from "astro-icon"
+import {SITE_URL} from "./site.config.mjs"
 
 export const darkMode = "media"
-export const SITE_URL = "https://astro.build" // set site URL and update site below also
-export const SITE_TITLE = "Desdinova"
-export const SITE_DESCRIPTION = "Built using Desdinova in Astro"
-// SITE_BACKGROUND_COLOR is color of canvas behind everything else
-export const SITE_BACKGROUND_COLOR = "bg-white dark:bg-gray-800"
-export const ACTIVE_LINK =
-    "flex items-center justify-center py-2 pr-4 pl-3 text-white rounded lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-export const INACTIVE_LINK =
-    "flex items-center justify-center py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro.build",
+    site: SITE_URL,
     integrations: [
         mdx(),
         tailwind({
---
+++ site.config.mjs
@@ -0,0 +1,8 @@
+export const SITE_URL = "https://astro.build" // set site URL and update site below also
+export const SITE_TITLE = "Desdinova"
+export const SITE_DESCRIPTION = "Built using Desdinova in Astro"
+// SITE_BACKGROUND_COLOR is color of canvas behind everything else
+export const SITE_BACKGROUND_COLOR = "bg-white dark:bg-gray-800"
+export const ACTIVE_LINK = "text-black dark:text-white"
+export const INACTIVE_LINK =
+    "text-gray-400 dark:text-gray-400 lg:text-primary-200"
