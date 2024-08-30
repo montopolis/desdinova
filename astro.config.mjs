3b1192f3dd332bfb1b6e0544d8affc99ff884c5a
Babul A. Mukherjee <babul@montopolis.com>
2024-08-30 09:02:21 -0500

    add site search

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import tailwind from "@astrojs/tailwind"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import icon from "astro-icon"
+import pagefind from "astro-pagefind"
 import siteData from "./src/config/siteData"
 
 export default defineConfig({
     site: siteData.url,
     trailingSlash: "never",
+    build: {
+        format: "file",
+    },
     integrations: [
         mdx(),
+        pagefind(),
         tailwind({
             // Example: Disable injecting a basic `base.css` import on every page.
             // Useful if you need to define and/or import your own custom `base.css`.
