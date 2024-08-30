6a3d8b5edea0bcea665276cdbfa1ed1aade75750
Babul A. Mukherjee <babul@montopolis.com>
2024-08-30 01:33:49 -0500

    document ascending/descending blog post date sort

--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -3,13 +3,17 @@ import type {CollectionEntry} from "astro:content"
 export const getPublishedAndSortedPosts = (
     allPosts: CollectionEntry<"blog">[],
 ) => {
-    return allPosts
-        .filter(post => {
-            return import.meta.env.PROD ? post.data.draft !== true : true
-        })
-        .sort(
-            (post1, post2) =>
-                post1.data.publishedDate.valueOf() -
-                post2.data.publishedDate.valueOf(),
-        )
+    return (
+        allPosts
+            .filter(post => {
+                return import.meta.env.PROD ? post.data.draft !== true : true
+            })
+            // (post1, post2) = ascending date sort
+            // (post2, post1) = descending date sort
+            .sort(
+                (post2, post1) =>
+                    post1.data.publishedDate.valueOf() -
+                    post2.data.publishedDate.valueOf(),
+            )
+    )
 }
