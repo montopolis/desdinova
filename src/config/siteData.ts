7fdef15d53617809c499a03507d6f5a8ccdb514f
Babul A. Mukherjee <babul@montopolis.com>
2024-09-01 22:32:26 -0500

    Refactor NavLink.astro

--- src/config/siteData.ts
+++ src/config/siteData.ts
@@ -22,10 +22,10 @@ export interface SiteDataProps {
 
     colors: {
         mainBackground: string
-        activeMenuClass: string
-        inactiveMenuClass: string
-        activeMenuIconClass: string
-        inactiveMenuIconClass: string
+        activeTopMenuClass: string
+        inactiveTopMenuClass: string
+        activeBottomMenuClass: string
+        inactiveBottomMenuClass: string
     }
 }
 // Update this file with your site specific information
@@ -69,17 +69,16 @@ const siteData: SiteDataProps = {
     colors: {
         // mainBackground is color of canvas behind everything else
         mainBackground: "bg-white dark:bg-gray-800",
-        // tailwind classes for active menu item
-        activeMenuClass:
-            "flex items-center justify-center py-2 pr-4 pl-3 text-black dark:text-white lg:text-primary-700",
-        // tailwind classes for inactive menu items
-        inactiveMenuClass:
-            "flex items-center justify-center py-2 pr-4 pl-3 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent text-gray-400 dark:text-gray-400",
-        // tailwind classes for active menu item icon
-        activeMenuIconClass: "mr-1 h-4 w-4 inline text-black dark:text-white",
-        // tailwind classes for inactive menu item icons
-        inactiveMenuIconClass:
-            "mr-1 h-4 w-4 inline text-gray-400 dark:text-gray-400",
+        // tailwind classes for top menu items
+        activeTopMenuClass:
+            "inline-flex w-full shrink-0 items-center justify-center text-center py-2 pl-3 pr-4 text-primary-700 dark:text-primary-400",
+        inactiveTopMenuClass:
+            "inline-flex w-full shrink-0 items-center justify-center text-center py-2 pl-3 pr-4 hover:bg-gray-100 hover:text-primary-700 lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent text-gray-500 dark:text-gray-400",
+        // tailwind classes for bottom menu items
+        activeBottomMenuClass:
+            "flex text-primary-700 dark:text-primary-400 hover:underline",
+        inactiveBottomMenuClass:
+            "hover:underline lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent text-gray-500 dark:text-gray-400",
     },
 }
 
