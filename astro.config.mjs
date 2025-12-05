8ecb834a0b89a03b20d3b2e63e53d3d3137ade0e
Babul A. Mukherjee <babul@montopolis.com>
2025-12-05 03:35:38 -0600

    upgrade to astro 5.16.4 & tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
@@ -12,14 +12,12 @@ export default defineConfig({
 	build: {
 		format: 'file',
 	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	integrations: [
 		mdx(),
 		pagefind(),
-		tailwind({
-			// Example: Disable injecting a basic `base.css` import on every page.
-			// Useful if you need to define and/or import your own custom `base.css`.
-			// applyBaseStyles: false,
-		}),
 		(await import('@playform/compress')).default({
 			// css already compressed
 			CSS: false,
